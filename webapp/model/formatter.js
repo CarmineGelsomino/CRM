647b05bb4453006fe9dad3886901d122aff417ae
carmine.gelsomino <carmine.gelsomino@accenture.com>
2026-03-12 18:07:00 +0100

    Update visualizzazione attivtà

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -28,6 +28,37 @@ sap.ui.define([], function () {
         lookupState: function (sKey, aItems) {
             var oItem = findCategoryInfo(sKey, aItems);
             return (oItem && oItem.state) || "None";
+        },
+
+        activityPriorityText: function (sPriority) {
+            var mLabels = {
+                bassa: "Bassa",
+                media: "Media",
+                alta: "Alta"
+            };
+
+            return mLabels[sPriority] || sPriority || "";
+        },
+
+        activityPriorityColorScheme: function (sPriority) {
+            var mSchemes = {
+                bassa: 8,
+                media: 7,
+                alta: 4
+            };
+
+            return mSchemes[sPriority] || 9;
+        },
+
+        activityTypeColorScheme: function (sType) {
+            var mSchemes = {
+                chiamata: 3,
+                evento: 5,
+                email: 9,
+                altro: 1
+            };
+
+            return mSchemes[sType] || 10;
         }
     };
 });
